d99af376300ade22ca14d30568c385294d7c87b6
Athul Ananth Ariga <athulananthariga@Athuls-MacBook-Air.local>
2026-03-15 17:57:58 +0530

    fix(deploy): Update Render Start Command to explicitly use Uvicorn and Gunicorn

--- main.py
+++ main.py
@@ -8,8 +8,3 @@ app.include_router(coding.router)
 app.include_router(admin.router)
 app.include_router(pages.router)
 
-if __name__ == "__main__":
-    import uvicorn
-    import os
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
